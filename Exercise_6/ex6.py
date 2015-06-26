28dc98e21ba55404549cfbaa031501773a9f1fd2
syfung <joshua.fung@mail.utoronto.ca>
2015-06-26 00:15:46 -0400

    Initial ADT of the exercise

---
+++ Exercise_6/ex6.py
@@ -0,0 +1,66 @@
+""" Exercise 6
+
+Joshua Fung
+June 25th, 2015
+
+Foure function:
+rsum: Reutrn sum
+rmax: Return max
+second_smallest: Return second smallest
+sum_max_min: Return the sum of max and min
+"""
+
+
+def rsum(in_list):
+    """(list) -> int
+
+    Return the sum of the list
+    """
+
+
+def rmax(in_list):
+    """(list) -> int
+
+    Return the max of the list
+    """
+
+
+def second_smallest(in_list, first_call=True):
+    """(list, bool) -> int/list
+
+    Return the second smallest of the list
+    """
+
+    
+""" This is a more sensible implmentation of the second_smallest but I
+don't like it. So it is commented out.
+
+
+def second_smallest(in_list):
+    ""(list) -> int
+
+    Return the second smallest of the list
+    ""
+
+
+def _sec_sma_helper(in_list):
+    ""(list) -> list
+
+    Return [smallest, second smallest]
+    ""
+"""
+
+    
+
+def sum_max_min(in_list):
+    """(list) -> int
+
+    Return the sum of max and min
+    """
+
+
+def _sum_helper(in_list):
+    """(list) -> list
+    
+    Return [max, min]
+    """
