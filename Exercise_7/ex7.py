e2927052c6be2b3c141065db9953325008a00b3d
Fung Joshua See Yui <c5fungjo@cdf.toronto.edu>
2015-06-30 18:29:37 -0400

    Inital plan for ex7.py

---
+++ Exercise_7/ex7.py
@@ -0,0 +1,23 @@
+""" CSC148 Exercise 7
+
+Joshua Fung
+June 30th, 2015
+
+Four Functions:
+rsum: Return sum
+rmax: Return max
+second_smallest: Return second smallest
+sum_max_min: Return the sum of max and min
+
+The different of this exercise than exercise_6 is that instead of
+working with a single list of intergers it needs to work with a
+combination of nested list and list.
+
+For example:
+[1, 2, 3]
+[1, [2, 3]]
+[[1], [2, [3]], [1]]
+"""
+
+
+# For now we will start with coping the old code form ex6.py
