ffbd9006fb16d49acd5b930e06558228b1eaacb8
Fung Joshua See Yui <c5fungjo@cdf.toronto.edu>
2015-07-21 18:17:48 -0400

    The inital code and the tpye contrace for the requied function

---
+++ Exercise_10/ex10.py
@@ -0,0 +1,51 @@
+class BTNode(object):
+    """A node in a binary tree."""
+
+    def __init__(self, value, left=None, right=None):
+        """(BTNode, int, BTNode, BTNode) -> NoneType
+        Initialize this node to store value and have children left and right,
+        as well as depth 0.
+        """
+        self.value = value
+        self.left = left
+        self.right = right
+        self.depth = 0  # the depth of this node in a tree
+
+    def __str__(self):
+        return self._str_helper("")
+
+    def _str_helper(self, indentation=""):
+        """(BTNode, str) -> str
+        Return a "sideways" representation of the subtree rooted at this node,
+        with right subtrees above parents above left subtrees and each node on
+        its own line, preceded by as many TAB characters as the node's depth.
+        """
+        ret = ""
+
+        if(self.right is not None):
+            ret += self.right._str_helper(indentation + "\t") + "\n"
+        ret += indentation + str(self.value) + "\n"
+        if(self.left is not None):
+            ret += self.left._str_helper(indentation + "\t") + "\n"
+        return ret
+
+    def set_depth(self, depth=0):
+        """(BTNode, int) -> NoType
+        Set the depth of the node the method belongs to. Also set the
+        depth for the children node recursively to the depth puls level
+        """
+
+    def leaves_andinternals(self):
+        """(BTNode) -> tuple
+        """
+
+    def sum_to_deepest(self):
+        """(BTNode) -> int
+        """
+
+
+if(__name__ == "__main__"):
+    # just a simple tree to practice on
+    my_tree = BTNode(10, BTNode(3, BTNode(5), BTNode(2)),
+                     BTNode(7, BTNode(4, BTNode(9)), BTNode(6)))
+    print(my_tree)
